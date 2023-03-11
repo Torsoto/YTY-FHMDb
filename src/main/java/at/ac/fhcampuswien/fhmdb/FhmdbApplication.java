561b49560e15624cbd2a751085296bf3005baf2d
YusufYrkl <yusuf.yuerekli@stud.fh-campuswien.ac.at>
2023-03-11 13:50:03 +0100

    b

--- src/main/java/at/ac/fhcampuswien/fhmdb/FhmdbApplication.java
+++ src/main/java/at/ac/fhcampuswien/fhmdb/FhmdbApplication.java
@@ -17,7 +17,6 @@ public class FhmdbApplication extends Application {
         stage.setTitle("YTY-FHMDb");
         stage.setScene(scene);
         stage.show();
-
     }
 
     public static void main(String[] args) {
