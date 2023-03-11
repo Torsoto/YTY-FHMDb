f006a21467b9824cb0ef22dc920d3d541071c294
TolgaT <tolga.topal@stud.fh-campuswien.ac.at>
2023-03-11 13:14:11 +0100

    added movies and genres

--- src/main/java/at/ac/fhcampuswien/fhmdb/models/Movie.java
+++ src/main/java/at/ac/fhcampuswien/fhmdb/models/Movie.java
@@ -1,30 +1,30 @@
 package at.ac.fhcampuswien.fhmdb.models;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Movie {
-    private String title;
+    private final String title;
     private String description;
 
-    private String genre;
+    private String[] genre;
 
     private static final String[] genres = {"ACTION", "ADVENTURE", "ANIMATION", "BIOGRAPHY", "COMEDY",
             "CRIME", "DRAMA", "DOCUMENTARY", "FAMILY", "FANTASY", "HISTORY", "HORROR",
             "MUSICAL", "MYSTERY", "ROMANCE", "SCIENCE_FICTION", "SPORT", "THRILLER", "WAR",
             "WESTERN"};
 
-    public Movie(String title, String description, String genre) {
+    public Movie(String title, String description, String[] genre) {
         this.title = title;
         this.description = description;
 
-        for (String s : genres) {
-            if (genre.equals(s)) {
-                System.out.println("genre already exists");
-                break;
-            }
-            else {
-                this.genre = genre;
+        for (int i = 0; i < genre.length; i++) {
+            for (int j = i; j < genres.length; j++){
+                if (genre[i].equals(genres[j])) {
+                    this.genre = genre;
+                    break;
+                }
             }
         }
     }
@@ -47,17 +47,32 @@ public class Movie {
     }
 
     public static List<Movie> initializeMovies(){
+        Movie SHAWSHANK = new Movie("The Shawshank Redemption", "A powerful story of hope, friendship, and the human spirit set against the backdrop of a brutal prison system.",new String[]{"DRAMA","CRIME"});
+        Movie LOTR1 = new Movie("The Lord of the Rings: The Fellowship of the Ring", "Frodo must destroy a powerful ring with the help of a fellowship on a dangerous journey", new String[]{"FANTASY","ADVENTURE"});
+        Movie LOTR2 = new Movie("The Lord of the Rings: The Two Towers", "The fellowship is split and faces new challenges, while Frodo and his Companion Sam are pursued by Gollum", new String[]{"FANTASY","ADVENTURE"});
+        Movie LOTR3 = new Movie("The Lord of the Rings: The Return of the King", "the final battle for Middle-earth begins as Frodo and Sam continue their perilous journey to destroy the ring", new String[]{"FANTASY","ADVENTURE"});
+        Movie INCEPTION = new Movie("Inception", "A skilled thief is hired to plant an idea into the mind of a corporate heir. As he and his team delve deeper into the man's subconscious, they must face dangerous adversaries and navigate complex layers of reality.", new String[]{"ACTION", "THRILLER", "SCI-FI"});
+        Movie PULPFICTION = new Movie("Pulp Fiction", "The lives of two mob hitmen, a boxer, a gangster's wife, and a pair of diner bandits intertwine in four tales of violence and redemption.", new String[]{"CRIME", "DRAMA"});
+        Movie FORRESTGUMP = new Movie("Forrest Gump", "Forrest Gump, a simple man with a low IQ, leads an extraordinary life and becomes a witness to and participant in some of the defining moments of the 20th century.", new String[]{"DRAMA", "ROMANCE", "COMEDY"});
         List<Movie> movies = new ArrayList<>();
-        Movie test = new Movie("The Shawshank Redemption", "In 1947, Andy Dufresne (Tim Robbins), a banker in Maine, is convicted of murdering his wife and her lover, a golf pro. Since the state of Maine has no death penalty, he is given two consecutive life sentences and sent to the notoriously harsh Shawshank Prison.","yo");
-        movies.add(test);
+        Movie INTERSTELLAR = new Movie("Interstellar", "A group of explorers embark on a mission to save humanity from a dying Earth by traveling through a wormhole to find a new habitable planet in another galaxy.", new String[]{"SCI-FI", "DRAMA"});
+        Movie SHUTTERISLAND = new Movie("Shutter Island", "In 1954, U.S. Marshal Teddy Daniels investigates the disappearance of a murderess from an asylum on Shutter Island and uncovers a web of deceit.", new String[]{"MYSTERY", "THRILLER"});
+        Movie TAXIDRIVER = new Movie("Taxi Driver", "A mentally unstable Vietnam War veteran works as a nighttime taxi driver in New York City, where the perceived decadence and sleaze fuels his urge for violent action.", new String[]{"DRAMA", "CRIME"});
+        Movie KUNGFUPANDA1 = new Movie("Kung Fu Panda", "In ancient China, a clumsy and overweight panda named Po dreams of becoming a kung fu master and is unexpectedly chosen to fulfill an ancient prophecy.", new String[]{"ANIMATION", "ACTION", "COMEDY"});
+        Movie KUNGFUPANDA2 = new Movie("Kung Fu Panda 2", "Po must confront a new and powerful villain who is using a secret weapon to conquer China and destroy kung fu.", new String[]{"ANIMATION", "ACTION", "COMEDY"});
+        Movie KUNGFUPANDA3 = new Movie("Kung Fu Panda 3", "Po reunites with his long-lost father and discovers a secret village of pandas, but an ancient and powerful villain threatens to destroy their peaceful existence.", new String[]{"ANIMATION", "ACTION", "COMEDY"});
+        Movie PARASITE = new Movie("Parasite", "A poor family scheming to become the servants of a wealthy family gradually infiltrates their household, but an unexpected incident sets off a series of events that expose their greed and deception.", new String[]{"THRILLER", "DRAMA"});
 
 
-        return movies;
+        Movie[] movieArray = {SHAWSHANK, LOTR1, LOTR2, LOTR3, INCEPTION, PULPFICTION, FORRESTGUMP, INTERSTELLAR, SHUTTERISLAND, TAXIDRIVER, KUNGFUPANDA1, KUNGFUPANDA2, KUNGFUPANDA3};
+        Arrays.sort(movieArray, (m1, m2) -> m1.getTitle().compareTo(m2.getTitle()));
+        return new ArrayList<>(Arrays.asList(movieArray));
     }
 
-    public String getGenre() {
+    public String[] getGenre() {
         return genre;
     }
+
 
     public static String[] getAllGenres() {
         return genres;
