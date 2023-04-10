74c870adcd12bebd466df34fdd73feba8e15b765
TolgaT <tolga.topal@stud.fh-campuswien.ac.at>
2023-04-10 17:48:24 +0200

    optimized movieAPI class (a lot of duplicate code)

--- src/main/java/at/ac/fhcampuswien/fhmdb/API/MovieAPI.java
+++ src/main/java/at/ac/fhcampuswien/fhmdb/API/MovieAPI.java
@@ -11,26 +11,76 @@ import java.util.List;
 import java.util.Objects;
 
 public class MovieAPI {
-    OkHttpClient client = new OkHttpClient();
-    String baseURL = "https://prog2.fh-campuswien.ac.at/movies";
+    private OkHttpClient client = new OkHttpClient();
 
-    Request request = new Request.Builder()
-            .url(baseURL)
-            .addHeader("User-Agent", "http.agent")
-            .build();
-    Call call = client.newCall(request);
-    Response response;
+    private Gson createGson(JsonDeserializer<Movie> movieDeserializer) {
+        return new GsonBuilder()
+                .registerTypeAdapter(Movie.class, movieDeserializer)
+                .create();
+    }
 
-    {
-        try {
-            response = call.execute();
+
+    private Gson getGsonWithMovieDeserializerForUI() {
+        return createGson((json, typeOfT, context) -> {
+            JsonObject jsonObject = json.getAsJsonObject();
+
+            String title = jsonObject.get("title").getAsString();
+            String description = jsonObject.get("description").getAsString();
+            JsonArray genreArray = jsonObject.getAsJsonArray("genres");
+            String[] genre = new Gson().fromJson(genreArray, String[].class);
+            int releaseYear = jsonObject.get("releaseYear").getAsInt();
+            String imgURL = jsonObject.get("imgUrl").getAsString();
+            double rating = jsonObject.get("rating").getAsDouble();
+
+            return new Movie(title, description, genre, rating, releaseYear, imgURL);
+        });
+    }
+
+    private Gson getGsonWithMovieDeserializer() {
+        return createGson((json, typeOfT, context) -> {
+            JsonObject jsonObject = json.getAsJsonObject();
+
+            String id = jsonObject.get("id").getAsString();
+            String title = jsonObject.get("title").getAsString();
+            String description = jsonObject.get("description").getAsString();
+            JsonArray genreArray = jsonObject.getAsJsonArray("genres");
+            String[] genre = new Gson().fromJson(genreArray, String[].class);
+            int releaseYear = jsonObject.get("releaseYear").getAsInt();
+            String imgURL = jsonObject.get("imgUrl").getAsString();
+            int lengthInMinutes = jsonObject.get("lengthInMinutes").getAsInt();
+            JsonArray directorsArray = jsonObject.getAsJsonArray("directors");
+            String director = new Gson().fromJson(directorsArray.get(0), String.class);
+            JsonArray writersArray = jsonObject.getAsJsonArray("writers");
+            List<String> writers = new Gson().fromJson(writersArray, new TypeToken<List<String>>() {}.getType());
+            JsonArray mainCastArray = jsonObject.getAsJsonArray("mainCast");
+            List<String> mainCast = new Gson().fromJson(mainCastArray, new TypeToken<List<String>>() {}.getType());
+            double rating = jsonObject.get("rating").getAsDouble();
+
+            return new Movie(title, description, genre, rating, releaseYear, mainCast, director, id, imgURL, writers, lengthInMinutes);
+        });
+    }
+
+    private Request createRequest(String url) {
+        return new Request.Builder()
+                .url(url)
+                .addHeader("User-Agent", "http.agent")
+                .build();
+    }
+
+    private <T> T executeRequest(Request request, Type type, Gson gson) {
+        try (Response response = client.newCall(request).execute()) {
+            if (response.isSuccessful()) {
+                return gson.fromJson(response.body().charStream(), type);
+            } else {
+                throw new IOException("Unexpected response: " + response);
+            }
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
     }
 
-    //Builds URL to get specific data from API
     public String buildURL(String query, String genre, Integer releaseYear, Double ratingFrom) {
+        String baseURL = "https://prog2.fh-campuswien.ac.at/movies";
         HttpUrl.Builder urlBuilder = Objects.requireNonNull(HttpUrl.parse(baseURL)).newBuilder();
 
         if (query != null) {
@@ -54,126 +104,20 @@ public class MovieAPI {
         return "http://prog2.fh-campuswien.ac.at/movies/" + movieId;
     }
 
-// Creates a Gson object with the registered Movie deserializer
-    private Gson getGsonWithMovieDeserializerForUI() {
-        // Instantiate a GsonBuilder
-        GsonBuilder gsonBuilder = new GsonBuilder();
-        // Register the custom Movie deserializer with the GsonBuilder
-        gsonBuilder.registerTypeAdapter(Movie.class, (JsonDeserializer<Movie>) (json, typeOfT, context) -> {
-            // Get the JSON object representing the movie
-            JsonObject jsonObject = json.getAsJsonObject();
-
-            // Extract fields from the JSON object
-            String title = jsonObject.get("title").getAsString();
-            String description = jsonObject.get("description").getAsString();
-            JsonArray genreArray = jsonObject.getAsJsonArray("genres");
-            String[] genre = new Gson().fromJson(genreArray, String[].class);
-            int releaseYear = jsonObject.get("releaseYear").getAsInt();
-            String imgURL = jsonObject.get("imgUrl").getAsString();
-            double rating = jsonObject.get("rating").getAsDouble();
-
-            // Create and return a new Movie object with the extracted fields
-            return new Movie(title, description, genre, rating, releaseYear, imgURL);
-        });
-        // Build and return the Gson object
-        return gsonBuilder.create();
-    }
-
-    private Gson getGsonWithMovieDeserializer() {
-        // Instantiate a GsonBuilder
-        GsonBuilder gsonBuilder = new GsonBuilder();
-        // Register the custom Movie deserializer with the GsonBuilder
-        gsonBuilder.registerTypeAdapter(Movie.class, (JsonDeserializer<Movie>) (json, typeOfT, context) -> {
-            // Get the JSON object representing the movie
-            JsonObject jsonObject = json.getAsJsonObject();
-
-            // Extract fields from the JSON object
-            String id = jsonObject.get("id").getAsString();
-            String title = jsonObject.get("title").getAsString();
-            String description = jsonObject.get("description").getAsString();
-            JsonArray genreArray = jsonObject.getAsJsonArray("genres");
-            String[] genre = new Gson().fromJson(genreArray, String[].class);
-            int releaseYear = jsonObject.get("releaseYear").getAsInt();
-            String imgURL = jsonObject.get("imgUrl").getAsString();
-            int lengthInMinutes = jsonObject.get("lengthInMinutes").getAsInt();
-            JsonArray directorsArray = jsonObject.getAsJsonArray("directors");
-            String director = new Gson().fromJson(directorsArray.get(0), String.class);
-            JsonArray writersArray = jsonObject.getAsJsonArray("writers");
-            List<String> writers = new Gson().fromJson(writersArray, new TypeToken<List<String>>() {}.getType());
-            JsonArray mainCastArray = jsonObject.getAsJsonArray("mainCast");
-            List<String> mainCast = new Gson().fromJson(mainCastArray, new TypeToken<List<String>>() {}.getType());
-            double rating = jsonObject.get("rating").getAsDouble();
-
-            // Create and return a new Movie object with the extracted fields
-            return new Movie(title, description, genre, rating, releaseYear, mainCast, director, id, imgURL, writers, lengthInMinutes);
-        });
-        // Build and return the Gson object
-        return gsonBuilder.create();
-    }
-
     public List<Movie> fetchMovies(String query, String genre, Integer releaseYear, Double ratingFrom, boolean UI) {
-        // Build the URL using the search parameters (query, genre, releaseYear, ratingFrom)
         String url = buildURL(query, genre, releaseYear, ratingFrom);
+        Request request = createRequest(url);
+        Gson gson = UI ? getGsonWithMovieDeserializerForUI() : getGsonWithMovieDeserializer();
+        Type movieListType = new TypeToken<List<Movie>>() {}.getType();
 
-        // Create a request object with the URL and a custom User-Agent header
-        Request request = new Request.Builder()
-                .url(url)
-                .addHeader("User-Agent", "http.agent")
-                .build();
-
-        // Execute the request using OkHttpClient (client) and retrieve the response
-        try (Response response = client.newCall(request).execute()) {
-            // Check if the response is successful
-            if (response.isSuccessful()) {
-                //use custom deserializer
-                Gson gson;
-                //depending on what constructor user wants to use choose different deserializer
-                if (UI){
-                    gson = getGsonWithMovieDeserializerForUI();
-                }else {
-                    gson = getGsonWithMovieDeserializer();
-                }
-                Type movieListType = new TypeToken<List<Movie>>() {
-                }.getType();
-                return gson.fromJson(response.body().charStream(), movieListType);
-            } else {
-                // If the response is not successful, throw an IOException with an error message
-                throw new IOException("Unexpected response: " + response);
-            }
-        } catch (IOException e) {
-            // If an exception occurs, wrap it in a RuntimeException and rethrow
-            throw new RuntimeException(e);
-        }
+        return executeRequest(request, movieListType, gson);
     }
-    // Fetches a movie by its unique ID
+
     public Movie fetchMovieById(String movieId, boolean UI) {
-        // Build the URL for fetching the movie by ID
         String url = buildMovieByIdURL(movieId);
-        // Create a request object with the URL and User-Agent header
-        Request request = new Request.Builder()
-                .url(url)
-                .addHeader("User-Agent", "http.agent")
-                .build();
-        // Execute the request and process the response
-        try (Response response = client.newCall(request).execute()) {
-            // Check if the response is successful
-            if (response.isSuccessful()) {
-                Gson gson;
-                //depending on what constructor user wants to use choose different deserializer
-                if (UI){
-                    gson = getGsonWithMovieDeserializerForUI();
-                }else {
-                    gson = getGsonWithMovieDeserializer();
-                }
-                // Deserialize the response body into a Movie object
-                return gson.fromJson(response.body().charStream(), Movie.class);
-            } else {
-                // If the response is not successful, throw an IOException
-                throw new IOException("Unexpected response: " + response);
-            }
-        } catch (IOException e) {
-            // If an exception occurs, wrap it in a RuntimeException and rethrow
-            throw new RuntimeException(e);
-        }
+        Request request = createRequest(url);
+        Gson gson = UI ? getGsonWithMovieDeserializerForUI() : getGsonWithMovieDeserializer();
+
+        return executeRequest(request, Movie.class, gson);
     }
 }
