af234b697cf6161b602a050493adfe8d92b6923c
Yunus Kara <yunuskr40@gmail.com>
2023-05-06 14:02:03 +0200

    Added WachlistRepository

--- src/main/java/at/ac/fhcampuswien/fhmdb/DataLayer/WatchlistRepository.java
+++ src/main/java/at/ac/fhcampuswien/fhmdb/DataLayer/WatchlistRepository.java
@@ -1,5 +1,32 @@
 package at.ac.fhcampuswien.fhmdb.DataLayer;
 
-public class WatchlistRepository {
+import at.ac.fhcampuswien.fhmdb.models.Movie;
+import com.j256.ormlite.dao.Dao;
 
+import java.sql.SQLException;
+import java.util.List;
+
+public class WatchlistRepository {
+    Dao<WatchlistMovieEntity, Long> dao;
+    WatchlistMovieEntity entityMethodCall = new WatchlistMovieEntity();
+    public WatchlistRepository(){
+        this.dao = Database.getDatabase().getWatchlistDao();
+    }
+
+    public void addToWatchlist(Movie movie) throws SQLException {
+        dao.create(movieToEntity(movie));
+    }
+
+    public List<WatchlistMovieEntity> readAllMovies() throws SQLException{
+        return dao.queryForAll();
+    }
+
+    public void removeFromWatchlist(Movie movie) throws SQLException {
+        dao.create(movieToEntity(movie));
+    }
+
+    private WatchlistMovieEntity movieToEntity(Movie movie){
+        return new WatchlistMovieEntity(movie.getID(), movie.getTitle(), movie.getDescription(), entityMethodCall.genreToString(movie.getGenre()),
+                movie.getReleaseYear(), movie.getImgURL(),movie.getLength(), movie.getRating());
+    }
 }
